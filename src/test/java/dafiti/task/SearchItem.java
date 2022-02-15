c30b2dc49ae362d1965d97bb081af85aae0dac4a
monsalvike <monsalvike@gmail.com>
2022-02-14 19:41:33 -0500

    tercer Commit - modifica SearchItem

--- src/test/java/dafiti/task/SearchItem.java
+++ src/test/java/dafiti/task/SearchItem.java
@@ -1,9 +1,12 @@
 package dafiti.task;
 
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.conditions.Check;
+import net.serenitybdd.screenplay.ensure.Ensure;
 
 import static dafiti.userinterfaces.Home.*;
 
@@ -21,6 +24,9 @@ public class SearchItem implements Task {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+        if actor.attemptsTo(Ensure.that(BTNGOTHEAPP.isVisibleFor(actor)).isTrue())  {
+
         }
         actor.attemptsTo(Click.on(BTNGOTHEAPP));
         actor.attemptsTo(Click.on(CLICKSEARCH));
